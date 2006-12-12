0186ffbdd3c7b7ca0d472f5d59bada37d7b0deb4
Joe Esposito <joe@joeyespo.com>
2006-12-12 00:00:00 -0500

    First commit of working project.

---
+++ [source]/WindowFinderDesigner.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.Design;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace WindowFinder
+{
+  public class WindowFinderDesigner : ControlDesigner
+  {
+    public override SelectionRules SelectionRules
+    { get { return (SelectionRules.Moveable | SelectionRules.Visible); } }
+  }
+}
